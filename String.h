6e2781fbc134ccc5f88f3617efec0718d986f9c7
jadecubes <debra321@hotmail.com>
2017-03-05 15:09:38 +0800

    Add files via upload
    PortableSock.h
    - add socket interface in common on different platform.
    
    String.*
    - include string related operations
    
    adapter.*
    - retrieve adapter info.

---
+++ String.h
@@ -0,0 +1,97 @@
+//---------------------------------------------------------------------------
+
+#ifndef String_H
+#define String_H
+/*
+ * author: Yu-Fang Juan
+ * date: Mar 5th, 2017
+ * title: Ticking Circus
+ * All rights reserved.
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#define IPSTR_LEN  50
+#define MACSTR_LEN 50
+
+
+#include <iostream>
+#include <string>
+#include <sstream>
+using namespace std;
+
+
+/*
+   unsigned int ip_int_cvt(IN char* ipadd)
+   convert the input ip address in the str format to be long format.
+*/
+unsigned int ip_int_cvt(char* ipadd);
+
+/*
+   void port_int2carray(OUT char* buf, IN int portint)
+   convert the int to be hex format.
+*/
+void port_int2carray(char* buf,int portint);
+
+
+/*
+   unsigned int port_carray2int(IN int len, IN char* buf)
+   convert the port in net byte order to be an int, which is the return value. len means the length of buf required to convert.
+*/
+unsigned int port_carray2int(int len,char* buf);
+
+
+/*
+   char* trim(OUT char* desc, IN char* src,IN char* seps)
+*/
+char* trim(char* desc,char* src,char* seps);
+
+
+/*
+   int checkIPAddr(IN char const* ipadd)
+   check the input ipadd is in valid ip format or not. 
+   0 returned: valid
+   -1 returned: invalid
+
+*/
+int checkIPAddr(char const* input);
+
+
+/*
+    void parseIPtoIntArray(OUT char* intsaver, IN char* IPArg)
+    convert the ip string to the hex format
+    e.g.
+    192.168.2.1=> intsaver [0]=0xc0, intsaver [1]=a8, intsaver [2]=0x02, intsaver [3]=0x01
+*/
+void parseIPtoIntArray(char* intsaver,char* IP);
+
+/*
+    void parseHexIPtoStr(OUT char* strip,IN char const* hexIP)
+    convert the ip in hex format to be in string format
+    hexIP [0]=0xc0, hexIP [1]=a8, hexIP [2]=0x02, hexIP [3]=0x01=> 192.168.2.1
+*/
+void parseHexIPtoStr(char* strip,char const* hexIP);
+int checkMask(char* mask);
+
+bool contains(char* str, char* token);
+bool isValidPort(int port);
+int getSpecialCharCountinSTR(char target,char const* str);
+int ascii2Hex(char* buf,int digitnum);
+unsigned int strToIntWithBase(int base,char* strStart,char* strFin);
+void str2Lower(char* string);
+void str2Upper(char* string);
+void uint2carray_given_len(unsigned int portint,int numlen,char* buf);
+
+int isAllEnChar(char* allchar,unsigned int len,unsigned int &index);
+int isAllMyChar(char spe,char* allchar,unsigned int len,unsigned int &index);
+void hex2Ascii(char hint,char* hstr);
+
+
+wstring widen( const string& str );
+string narrow( const wstring& str );
+//---------------------------------------------------------------------------
+#endif
---
+++ adapter.cpp
@@ -0,0 +1,190 @@
+/*
+ * author: Yu-Fang Juan
+ * date: Mar 5th, 2017
+ * title: Ticking Circus
+ * All rights reserved.
+ */
+#include "adapter.h"
+
+#include <winsock2.h>
+#include <iphlpapi.h>
+#include <iostream>
+
+typedef unsigned char u8_t;
+typedef unsigned short u16_t;
+typedef unsigned long u32_t;
+typedef char  s8_t;
+typedef short s16_t;
+typedef long  s32_t;
+
+
+domain::domain(){
+  memset(domain_name,0,sizeof(domain_name));
+  memset(host_name,0,sizeof(host_name));
+  memset(dns,0,sizeof(dns));
+}
+int getDomainInfo(domain* p_dm){
+   ULONG ulLen1=4096;
+   char pbBuf1[4096];
+   
+   IP_ADDR_STRING *pIPAddr;
+   if(ERROR_SUCCESS==GetNetworkParams((FIXED_INFO*)pbBuf1, &ulLen1))
+   {
+	 FIXED_INFO* FixedInfo=(FIXED_INFO*)pbBuf1;
+	 switch(FixedInfo->NodeType)
+     {       
+		case     BROADCAST_NODETYPE:
+		break;
+		case     PEER_TO_PEER_NODETYPE:
+		break;
+		case     MIXED_NODETYPE:
+		break;
+		case     HYBRID_NODETYPE:
+		break;
+		default:
+		break;
+	 }
+	 strcpy(p_dm->domain_name,FixedInfo->DomainName);
+	 strcpy(p_dm->host_name,FixedInfo->HostName);
+	 parseIPtoIntArray(&(p_dm->dns[0][0]),FixedInfo->DnsServerList.IpAddress.String);
+	 pIPAddr=FixedInfo->DnsServerList.Next;
+	 parseIPtoIntArray(&(p_dm->dns[1][0]),	pIPAddr->IpAddress.String);
+	 return 0;
+   }
+   else{
+	 return -1;
+   }
+
+}
+
+
+/*
+
+    int getAdapterInfo(OUT deque<Adapter> &macdq)
+    0 returned: successful
+    -1 returned: unsuccessful
+*/
+int getAdapterInfo(adapter_container &adaq){
+   for(size_t i=0;i<adaq.size();i++){
+	 delete adaq.at(i);
+   }
+   adaq.clear();
+   ULONG ulLen1=4096;
+   char pbBuf1[4096];
+   Adapter* curadapter=new Adapter();
+   if(ERROR_SUCCESS==GetAdaptersInfo((IP_ADAPTER_INFO*)pbBuf1, &ulLen1))
+   {
+	  char* macbuf=curadapter->macadd;
+	  char* desc=curadapter->desc;
+	  char* name=curadapter->name;
+      char* mask=curadapter->mask;
+	  
+	  PIP_ADAPTER_INFO pAdapterInfo,pAdapter=0;
+	  pAdapterInfo=(IP_ADAPTER_INFO*)pbBuf1;
+	  pAdapter = pAdapterInfo;
+	  while(pAdapter){
+		 for(unsigned int i=0;i<pAdapterInfo->AddressLength;i++){
+		   macbuf[i]=pAdapter->Address[i];
+		 }
+		 strcpy(desc,pAdapter->Description);
+		 strcpy(name,pAdapter->AdapterName);
+		 parseIPtoIntArray(mask,pAdapter->IpAddressList.IpMask.String);
+		 adaq.push_back(curadapter);
+		 pAdapter = pAdapter->Next;
+	  }
+	  return 0;
+   }
+   else{
+		return -1;
+   }    
+
+}
+
+
+
+/*
+    int getBcastAddPerAdapter(OUT deque<u32_t> &adaq)
+    gets all broadcast address from all adapters on the current PC, and saves them into the adaq.
+    0 returned: successful
+   -1 returned: unsuccessful
+*/
+int getBcastAddPerAdapter(num_container &bcastq){
+   bcastq.clear();
+   ULONG ulLen1=4096;
+   char pbBuf1[4096];
+   if(ERROR_SUCCESS==GetAdaptersInfo((IP_ADAPTER_INFO*)pbBuf1, &ulLen1))
+   {
+	  char ipaddrcopy[IPSTR_LEN];
+	  char maskcopy[IPSTR_LEN];
+	  
+	  PIP_ADAPTER_INFO pAdapterInfo,pAdapter=0;
+	  pAdapterInfo=(IP_ADAPTER_INFO*)pbBuf1;
+	  pAdapter = pAdapterInfo;
+	  while(pAdapter){
+	   IP_ADDR_STRING *ipstr=&(pAdapter->IpAddressList);
+	   while(ipstr!=NULL){
+		 strcpy(ipaddrcopy,ipstr->IpAddress.String);
+		 strcpy(maskcopy,pAdapter->IpAddressList.IpMask.String);
+		 
+		 if(!strcmp(ipaddrcopy,"0.0.0.0"))
+		 {
+		 }
+		 else
+		 {
+			u32_t IPaddr=inet_addr(ipaddrcopy);
+			u32_t mask=inet_addr(maskcopy);
+			u32_t bcastaddr=(IPaddr & mask)| (~mask) ;
+			bcastq.push_back(bcastaddr);
+		  }
+		  ipstr=ipstr->Next;
+		}
+		pAdapter = pAdapter->Next;
+	  }
+       return 0;
+   }
+   else{
+        return -1;
+   }
+}
+
+
+/*
+    int getLocalIPList(OUT deque<u32_t> &ipq)
+*/
+int getLocalIPList(num_container &ipq){
+   ipq.clear();
+   ULONG ulLen1=4096;
+   char pbBuf1[4096];
+
+   if(ERROR_SUCCESS==GetAdaptersInfo((IP_ADAPTER_INFO*)pbBuf1, &ulLen1))
+   {
+	  char ipaddrcopy[IPSTR_LEN];
+	  //
+	  PIP_ADAPTER_INFO pAdapterInfo,pAdapter=0;
+	  pAdapterInfo=(IP_ADAPTER_INFO*)pbBuf1;
+	  pAdapter = pAdapterInfo;
+	  while(pAdapter){
+		IP_ADDR_STRING *ipstr=&(pAdapter->IpAddressList);
+		while(ipstr!=NULL){
+		 strcpy(ipaddrcopy,ipstr->IpAddress.String);
+		 //
+		 if(!strcmp(ipaddrcopy,"0.0.0.0"))
+		 {
+		 }
+		 else
+		 {
+           u32_t uipadd= inet_addr(ipaddrcopy);
+		   ipq.push_back(uipadd);
+		 }
+		 ipstr=ipstr->Next;
+		}
+			pAdapter = pAdapter->Next;
+	  }
+	  return 0;
+   }
+   else{
+        return -1;
+   }
+}
+
+
